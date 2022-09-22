522b93bde8c3e9d62e4dd31253a712cdcd71881f
Izbanned <77387546+Izbanned@users.noreply.github.com>
2022-09-22 20:25:12 +0600

    Initial Commit

---
+++ GesterRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GesterRecognizer
+//
+//  Created by Dias Karimov on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
