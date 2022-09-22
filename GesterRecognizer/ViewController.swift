e04c759205a9337df78fb5cf54a89b0a44f820f0
Izbanned <77387546+Izbanned@users.noreply.github.com>
2022-09-22 20:50:47 +0600

    Swipes and taps recognized

--- GesterRecognizer/ViewController.swift
+++ GesterRecognizer/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        swipesObserves()
+        topObserver()
     }
 
-
+    func swipesObserves() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    func topObserver() {
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(tripleTapAction))
+        tripleTap.numberOfTapsRequired = 3
+        self.view.addGestureRecognizer(tripleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(doubleTapAction))
+        doubleTap.require(toFail: tripleTap)
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func handleSwipes(gester: UISwipeGestureRecognizer) {
+        switch gester.direction {
+        case .right:
+            textLabel.text = "Right"
+        case .left:
+            textLabel.text = "Left"
+        case .up:
+            textLabel.text = "Up"
+        case .down:
+            textLabel.text = "Down"
+        default:
+            break
+        }
+    }
+    
+    @objc func tripleTapAction() {
+        textLabel.text = "Triple Tap"
+    }
+    @objc func doubleTapAction() {
+        textLabel.text = "Double Tap"
+    }
 }
 
